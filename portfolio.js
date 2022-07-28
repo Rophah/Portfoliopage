66ba8e5b7c6faa7f92be84f6678494de3c76a825
Adebanjo Rafat <aderafat@gmail.com>
2022-07-28 18:05:50 +0100

    implements hamburger functionality with javaScript

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('#hamburger');
+const closed = document.querySelector('#close');
+const navListItems = document.querySelectorAll('.nav-list-item');
+const navList = document.querySelector('#nav-list');
+
+const mobileMenu = () => {
+  navList.classList.toggle('active');
+  hamburger.classList.toggle('close');
+  closed.classList.toggle('displayshow');
+};
+
+navListItems.forEach(element => {
+  element.addEventListener('click', mobileMenu);
+});
+
+hamburger.addEventListener('click', mobileMenu);
+closed.addEventListener('click', mobileMenu);
