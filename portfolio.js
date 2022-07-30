c3dbf1cb9d0ec38ec62802ea0fa8b63631d0d7f4
Adebanjo Rafat <aderafat@gmail.com>
2022-07-30 16:09:51 +0100

    creates some features in pop up

--- portfolio.js
+++ portfolio.js
@@ -15,3 +15,57 @@ navListItems.forEach((element) => {
 
 hamburger.addEventListener('click', mobileMenu);
 closed.addEventListener('click', mobileMenu);
+
+// local project data
+const projects = [
+  {
+    id: 1,
+    projectname: 'Tonic',
+    subprojectname: 'canopy',
+    projecttype: 'Back end dev',
+    year: 2015,
+    image: 'images/portoflioCard/facebook.png',
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    technology: ['html', 'css', 'javascript'],
+    projectsrc: '#',
+    githubsrc: '#',
+  },
+  {
+    id: 1,
+    projectname: 'Tonic',
+    subprojectname: 'canopy',
+    projecttype: 'Back end dev',
+    year: 2015,
+    image: 'images/portoflioCard/nature.png',
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    technology: ['html', 'css', 'javascript'],
+    projectsrc: '#',
+    githubsrc: '#',
+  },
+  {
+    id: 3,
+    projectname: 'Tonic',
+    subprojectname: 'canopy',
+    projecttype: 'Back end dev',
+    year: 2015,
+    image: 'images/portoflioCard/professional.png',
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    technology: ['html', 'css', 'javascript'],
+    projectsrc: '#',
+    githubsrc: '#',
+  },
+  {
+    id: 4,
+    projectname: 'Multi-Post Stories',
+    subprojectname: 'canopy',
+    projecttype: 'Back end dev',
+    year: 2015,
+    image: 'images/portoflioCard/uber.png',
+    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    technology: ['html', 'css', 'javascript'],
+    projectsrc: '#',
+    githubsrc: '#',
+  },
+];
+
+const image = document.getElementById('project-img');
