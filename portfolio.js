22bf36bcc305601e76d20cba2f324303aae1ec3a
Adebanjo Rafat <aderafat@gmail.com>
2022-07-31 13:17:32 +0100

    creates pop-up modal for work section

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,3 @@
-const hamburger = document.querySelector('#hamburger');
-const closed = document.querySelector('#close');
-const navListItems = document.querySelectorAll('.nav-list-item');
-const navList = document.querySelector('#nav-list');
-
-const mobileMenu = () => {
-  navList.classList.toggle('active');
-  hamburger.classList.toggle('close');
-  closed.classList.toggle('displayshow');
-};
-
-navListItems.forEach((element) => {
-  element.addEventListener('click', mobileMenu);
-});
-
-hamburger.addEventListener('click', mobileMenu);
-closed.addEventListener('click', mobileMenu);
-
 // local project data
 const projects = [
   {
@@ -25,20 +7,26 @@ const projects = [
     projecttype: 'Back end dev',
     year: 2015,
     image: 'images/portoflioCard/facebook.png',
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    technology: ['html', 'css', 'javascript'],
+    details:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    usedtechnology1: 'html',
+    usedtechnology2: 'css',
+    usedtechnology3: 'javascript',
     projectsrc: '#',
     githubsrc: '#',
   },
   {
     id: 1,
-    projectname: 'Tonic',
+    projectname: 'Multi-Post Stories',
     subprojectname: 'canopy',
     projecttype: 'Back end dev',
     year: 2015,
     image: 'images/portoflioCard/nature.png',
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    technology: ['html', 'css', 'javascript'],
+    details:
+      "images/portoflioCardLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    usedtechnology1: 'html',
+    usedtechnology2: 'css',
+    usedtechnology3: 'javascript',
     projectsrc: '#',
     githubsrc: '#',
   },
@@ -49,8 +37,11 @@ const projects = [
     projecttype: 'Back end dev',
     year: 2015,
     image: 'images/portoflioCard/professional.png',
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    technology: ['html', 'css', 'javascript'],
+    details:
+      "technology: ['html', 'css', 'javascript'],Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    usedtechnology1: 'html',
+    usedtechnology2: 'css',
+    usedtechnology3: 'javascript',
     projectsrc: '#',
     githubsrc: '#',
   },
@@ -61,11 +52,93 @@ const projects = [
     projecttype: 'Back end dev',
     year: 2015,
     image: 'images/portoflioCard/uber.png',
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    technology: ['html', 'css', 'javascript'],
+    details:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    usedtechnology1: 'html',
+    usedtechnology2: 'css',
+    usedtechnology3: 'javascript',
     projectsrc: '#',
     githubsrc: '#',
   },
 ];
 
-const image = document.getElementById('project-img');
+const hamburger = document.querySelector('#hamburger');
+const closed = document.querySelector('.close');
+const navListItems = document.querySelectorAll('.nav-list-item');
+const navList = document.querySelector('#nav-list');
+
+const mobileMenu = () => {
+  navList.classList.toggle('active');
+  hamburger.classList.toggle('close');
+  closed.classList.toggle('displayshow');
+};
+
+navListItems.forEach(element => {
+  element.addEventListener('click', mobileMenu);
+});
+
+hamburger.addEventListener('click', mobileMenu);
+closed.addEventListener('click', mobileMenu);
+
+const images = document.getElementById('project-img');
+const details = document.getElementById('newdetail');
+const projectsrcs = document.getElementById('projectsrc');
+const projectnames = document.getElementById('projectname');
+const subprojectnames = document.getElementById('subprojectname');
+const years = document.getElementById('year');
+const frontendBackendDev = document.getElementById('frontend/backend');
+const technology1 = document.getElementById('tech1');
+const technology2 = document.getElementById('tech2');
+const technology3 = document.getElementById('tech3');
+const githubsrcs = document.getElementById('githubsrc');
+const button1 = document.getElementById('seeproject1');
+const button2 = document.getElementById('seeproject2');
+const button3 = document.getElementById('seeproject3');
+const button4 = document.getElementById('seeproject4');
+const closeBtn = document.querySelector('.popUpClose');
+const modal = document.querySelector('.modal-overlay');
+
+let count = 0;
+
+const projectmodal = () => {
+  const project = projects[count];
+  images.src = project.image;
+  details.textContent = project.details;
+  projectsrcs.href = project.projectsrc;
+  projectnames.textContent = project.projectname;
+  subprojectnames.textContent = project.subprojectname;
+  years.textContent = project.year;
+  frontendBackendDev.textContent = project.projecttype;
+  githubsrcs.href = project.githubsrc;
+  technology1.textContent = project.usedtechnology1;
+  technology2.textContent = project.usedtechnology2;
+  technology3.textContent = project.usedtechnology3;
+};
+
+button1.addEventListener('click', () => {
+  count = 0;
+  projectmodal();
+  modal.classList.add('open-modal');
+});
+
+button2.addEventListener('click', () => {
+  count = 1;
+  projectmodal();
+  modal.classList.add('open-modal');
+});
+
+button3.addEventListener('click', () => {
+  count = 2;
+  projectmodal();
+  modal.classList.add('open-modal');
+});
+
+button4.addEventListener('click', () => {
+  count = 3;
+  projectmodal();
+  modal.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('open-modal');
+});
